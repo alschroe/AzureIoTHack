1af4222a52f9489f7a8920d7dcdf163652b0b639
alschroe <heinz_metzing@web.de>
2022-11-15 18:11:09 +0100

    terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "location" {
+  default     = "northeurope"
+  description = "Location of the resources"
+}
+
+variable "prefix" {
+  type        = string
+  default     = "<YOUR_PREFIX>"
+  description = "The prefix used for all resources"
+}
